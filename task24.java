9dc90209286774078a9d81f8f2955d81a9bceaa0
Dhiyani.S <dhiyanisrinivasan123@gmail.com>
2026-07-21 15:52:18 +0530

    Add functionality to find last occurrence of target
    Implement a program to find the last occurrence of a target element in an array.

---
+++ task24.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class task24{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number of element in an array:");
+        int n=sc.nextInt();
+        int [] a1=new int [n];
+        for(int i=0;i<n;i++){
+            System.out.println("Enter element "+(i+1)+": ");
+            a1[i]=sc.nextInt();
+        }
+        boolean found=false;
+        System.out.println("Enter the Target :");
+        int target=sc.nextInt();
+        {
+            for(int i=n-1;i>0;i--){
+            if(target==a1[i]){
+                found=true;
+                System.out.println(Arrays.toString(a1));
+                System.out.println("  last occurrence found at index"+i);
+                break;
+            }
+        }
+        if(!found){
+             System.out.println("Element not found ");
+        }
+    }
+}
+}
